9f045fe433280c59926731f79ee592a3a15ad7fc
Jonathan Tellez <jonathanbtellez@gmail.com>
2023-02-12 19:39:11 -0500

    Creating inventary front end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardRouterModule } from './modules/dashboard/dashboard-routing.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+      routes,
+      {
+        enableTracing: false,
+        useHash: true
+      }),
+      DashboardRouterModule
+    ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
